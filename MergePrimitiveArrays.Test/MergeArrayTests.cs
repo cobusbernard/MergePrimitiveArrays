a34bc407195a6041fed27e76905c573121f394a2
Cobus Bernard <cobusbernard@gmail.com>
2014-03-09 18:21:43 +0200

    Test results should be in descending order. Added helper method to parse array values to readable strings.

--- MergePrimitiveArrays.Test/MergeArrayTests.cs
+++ MergePrimitiveArrays.Test/MergeArrayTests.cs
@@ -1,21 +1,27 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MergePrimitiveArrays.Test
 {
     [TestClass]
     public class MergeArrayTests
     {
+        #region Tests
         [TestMethod]
         public void TestPlainMerge()
         {
             var aArray = new[] { 1, 3, 5 };
             var bArray = new[] { 2, 4, 6 };
 
-            int[] expectedArray = { 1, 2, 3, 4, 5, 6 };
+            int[] expectedArray = { 6, 5, 4, 3, 2, 1 };
 
             var result = MergeArrayUtils.MergeAndReorder(aArray, bArray);
 
-            CollectionAssert.AreEqual(expectedArray, result);
+            CollectionAssert.AreEqual(
+                expectedArray,
+                result,
+                string.Format("Expected {0}, but received {1}.", ArrayToString(expectedArray), ArrayToString(result))
+                );
         }
 
         [TestMethod]
@@ -24,11 +30,15 @@ namespace MergePrimitiveArrays.Test
             var aArray = new[] { 1 };
             var bArray = new[] { 2, 5, 6, 8, 9 };
 
-            int[] expectedArray = { 1, 2, 5, 6, 8, 9 };
+            int[] expectedArray = { 9, 8, 6, 5, 2, 1 };
 
             var result = MergeArrayUtils.MergeAndReorder(aArray, bArray);
 
-            CollectionAssert.AreEqual(expectedArray, result);
+            CollectionAssert.AreEqual(
+                expectedArray,
+                result,
+                string.Format("Expected {0}, but received {1}.", ArrayToString(expectedArray), ArrayToString(result))
+                );
         }
 
         [TestMethod]
@@ -37,11 +47,15 @@ namespace MergePrimitiveArrays.Test
             var aArray = new[] { 1 };
             var bArray = new[] { 2 };
 
-            int[] expectedArray = { 1, 2 };
+            int[] expectedArray = { 2, 1 };
 
             var result = MergeArrayUtils.MergeAndReorder(aArray, bArray);
 
-            CollectionAssert.AreEqual(expectedArray, result);
+            CollectionAssert.AreEqual(
+                expectedArray, 
+                result,
+                string.Format("Expected {0}, but received {1}.", ArrayToString(expectedArray), ArrayToString(result))
+                );
         }
 
         [TestMethod]
@@ -54,7 +68,11 @@ namespace MergePrimitiveArrays.Test
 
             var result = MergeArrayUtils.MergeAndReorder(aArray, bArray);
 
-            CollectionAssert.AreEqual(expectedArray, result);
+            CollectionAssert.AreEqual(
+                expectedArray,
+                result,
+                string.Format("Expected {0}, but received {1}.", ArrayToString(expectedArray), ArrayToString(result))
+                );
         }
 
         [TestMethod]
@@ -63,12 +81,26 @@ namespace MergePrimitiveArrays.Test
             var aArray = new[] { 1, 2, 3 };
             var bArray = new[] { 1, 2, 3 };
 
-            int[] expectedArray = { 1, 2, 3 };
+            int[] expectedArray = { 3, 2, 1 };
 
             var result = MergeArrayUtils.MergeAndReorder(aArray, bArray);
 
-            CollectionAssert.AreEqual(expectedArray, result);
+            CollectionAssert.AreEqual(
+                expectedArray,
+                result,
+                string.Format("Expected {0}, but received {1}.", ArrayToString(expectedArray), ArrayToString(result))
+                );
         }
 
+        #endregion
+
+        #region Helper Methods
+        private string ArrayToString(int[] array)
+        {
+            var result = array.Aggregate("", (current, value) => current + string.Format(", {0}", value));
+
+            return string.Format("[ {0} ]",result.Substring(2));
+        }
+        #endregion
     }
 }
