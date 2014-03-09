b2484098e937a9a036dbada1fa0617033c9e1ce9
Cobus Bernard <cobusbernard@gmail.com>
2014-03-09 18:45:10 +0200

    First version working and tests passing.

--- MergePrimitiveArrays/MergeArrayUtils.cs
+++ MergePrimitiveArrays/MergeArrayUtils.cs
@@ -4,9 +4,49 @@
     {
         public static int[] MergeAndReorder(int[] a, int[] b)
         {
-            var mergedArray = new[] {0};
+            var mergedArray = new int[a.Length + b.Length];
+            var mergeArrayCounter = 0;
+            var duplicateCounter = 0;
 
-            return mergedArray;
+            var aCounter = a.Length - 1;
+            var bCounter = b.Length - 1;
+
+            var valueToSet = 0;
+
+            do
+            {
+                if (aCounter > -1 && bCounter > -1)
+                {
+                    valueToSet = a[aCounter] > b[bCounter]
+                    ? a[aCounter--]
+                    : b[bCounter--];
+                }
+                else if (aCounter == 1)
+                {
+                    valueToSet = b[bCounter--];
+                }
+                else if (bCounter == -1)
+                {
+                    valueToSet = a[aCounter--];
+                }
+
+                if (mergeArrayCounter > 0 && mergedArray[mergeArrayCounter - 1] == valueToSet)
+                {
+                    duplicateCounter++;
+                    continue;
+                }
+                mergedArray[mergeArrayCounter++] = valueToSet;
+
+            } while (aCounter > -1 || bCounter > -1);
+
+            var newLength = mergedArray.Length - duplicateCounter;
+            var truncatedMergedArray = new int[newLength];
+            for (var i = 0; i < newLength; i++)
+            {
+                truncatedMergedArray[i] = mergedArray[i];
+            }
+
+            return truncatedMergedArray;
         }
 
         public static void Main(string[] args)
